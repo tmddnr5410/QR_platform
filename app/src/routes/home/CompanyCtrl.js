522d3d25212d0cd23b8a1ce6b7f77bf71442ee12
LeeSeungUk <tmddnr5410@gmail.com>
2023-07-25 16:08:27 +0900

    feat : 회사정보 확인가능, 방문기록 저장 추가

--- app/src/routes/home/CompanyCtrl.js
+++ app/src/routes/home/CompanyCtrl.js
@@ -14,7 +14,10 @@ class CompanyCtrl {
       const {name:cpnName , intro:cpnIntro} = data[0];
       //행의 data[0].name, data[0].intro를 받는 변수
       //{DB속성값 : 변수이름1, DB속성값 : 변수이름2}로 사용
+      
       HistoryStorage.save(userId,cpnID);
+      //userid, companyid 방문기록 저장
+
       res.render("home/show",{cpnName:cpnName,cpnIntro:cpnIntro});
     } else {
       res.redirect("/login");
