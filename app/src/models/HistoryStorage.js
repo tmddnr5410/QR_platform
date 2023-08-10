26850ab4a497b3ba33d58346e59cc3e0dd2adf12
LeeSeungUk <tmddnr5410@gmail.com>
2023-08-11 00:41:13 +0900

    feat :company model, 로그 출력 백엔드 코드추가

--- app/src/models/HistoryStorage.js
+++ app/src/models/HistoryStorage.js
@@ -5,6 +5,18 @@ const db = require('../config/db');
 //class명은 파일이름과 동일한게 좋음
 class HistoryStorage{
 
+
+    static async getHistory(){
+        return new Promise((resolve,reject)=>{
+            const query = "SELECT * from visithistory;";
+            
+            db.query(query,[],(err)=>{
+                if(err) throw reject(`${err}`);
+                resolve(data);
+            });
+        });
+    }
+
     static async save(uid,cpnid){
         return new Promise((resolve,reject) => {
             const query = "INSERT INTO visithistory(uid,cpnid) VALUES(?,?);";
