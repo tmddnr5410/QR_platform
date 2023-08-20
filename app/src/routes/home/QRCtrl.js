4b89e8280837492feb93bfb02a0125628bdad338
LeeSeungUk <tmddnr5410@gmail.com>
2023-08-20 16:46:43 +0900

    feat : QR 확인기능 추가

--- app/src/routes/home/QRCtrl.js
+++ app/src/routes/home/QRCtrl.js
@@ -6,8 +6,8 @@ class QRCtrl {
     }
 
     static madeQR(req,res){
-        const text = req.body.text;
-
+        const text = `http://localhost:3000/show/${req.params.cpnID}`;
+        console.log(text);
         QRCode.toDataURL(text,(err,url)=>{
             const data = url.replace(/.*,/,"");
             const img = new Buffer.from(data,"base64");
